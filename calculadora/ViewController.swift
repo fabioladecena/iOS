ccc9310fdd525d08fcebad64db6cd1c4a7ca7661
Alumno05 <alumno05@imac-de-administrador-54.local>
2021-02-18 19:22:01 +0100

    Initial Commit

---
+++ calculadora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculadora
+//
+//  Created by Alumno05 on 18/2/21.
+//  Copyright © 2021 Alumno05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
