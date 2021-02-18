552bae6004abe260b37d4fd866b4c52a3c553852
Alumno05 <alumno05@iMac-de-Administrador-54.local>
2021-02-18 20:35:03 +0100

    ejercicio calculadora

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var numero1: UITextField!
+    
+    @IBOutlet weak var numero2: UITextField!
+    
+    @IBOutlet weak var resultado: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    //MARK: Actions
+    @IBAction func btnSumar(_ sender: UIButton) {
+        resultado.text = String(Int(numero1.text!)! + Int(numero2.text!)!)
+    }
+    
+    @IBAction func btnRestar(_ sender: UIButton) {
+        resultado.text = String(Int(numero1.text!)! - Int(numero2.text!)!)
+    }
+    
+    @IBAction func btnMultiplicar(_ sender: UIButton) {
+        resultado.text = String(Int(numero1.text!)! * Int(numero2.text!)!)    }
+    
+    @IBAction func btnDividir(_ sender: UIButton) {
+        resultado.text = String(Int(numero1.text!)! / Int(numero2.text!)!)
+    }
 }
+
 
